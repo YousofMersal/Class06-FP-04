7065c6f6e417d1197eef7f152a4ee2dda4822f7d
Yousof Mersal <yousof777@gmail.com>
2018-12-02 15:27:03 +0100

    fix path to build folder in node server

--- src/server/index.js
+++ src/server/index.js
@@ -2,10 +2,18 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static(__dirname + '../../build'))
+const buildPath = path.join(__dirname, '../../build')
+
+app.use(express.static(buildPath))
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../../build', 'index.html'))
+  // res.sendFile(path.join(__dirname, '../../build', 'index.html'))
+  // res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  //res.sendFile(path.join(buildPath, 'index.html'))
+
+  const indexPath = path.join(buildPath, 'index.html')
+
+  res.sendFile(indexPath)
 })
 
 app.listen(process.env.PORT || 9001, () => {
