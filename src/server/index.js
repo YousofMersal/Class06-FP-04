0732e71dcebaf32a949ee96b74b695fd5d1b7d60
Yousof Mersal <yousof777@gmail.com>
2018-12-02 16:21:03 +0100

    send JSON file to browser

--- src/server/index.js
+++ src/server/index.js
@@ -1,19 +1,26 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 const app = express()
 
+const mentorsFilePath = path.join(__dirname, './api/Mentors.json')
 const buildPath = path.join(__dirname, '../../build')
 
 app.use(express.static(buildPath))
 
 app.get('/', function(req, res) {
-  // res.sendFile(path.join(__dirname, '../../build', 'index.html'))
-  // res.sendFile(path.join(__dirname, 'build', 'index.html'))
-  //res.sendFile(path.join(buildPath, 'index.html'))
-
   const indexPath = path.join(buildPath, 'index.html')
-
   res.sendFile(indexPath)
+})
+
+app.get('/mentors', (req, res) => {
+  fs.readFile(mentorsFilePath, 'utf8', (err, data) => {
+    try {
+      res.send(data)
+    } catch (error) {
+      throw new Error(err)
+    }
+  })
 })
 
 app.listen(process.env.PORT || 9001, () => {
