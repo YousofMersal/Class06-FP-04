da00ed4c86614bca493f8048919556729f3c61e9
Yousof Mersal <yousof777@gmail.com>
2018-12-03 02:45:18 +0100

    add fetch to react-app and catch-all in express for serving build folder && ground work for api calls

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  //adding mentors in state for displaying it later
+  constructor(props) {
+    super(props)
+    this.state = {
+      mentors: []
+    }
+  }
+
+  //fetch request to express backend
+  componentDidMount() {
+    fetch('/api/mentors')
+      .then(res => {
+        if (res.status !== 200) {
+          throw Error(res.statusText)
+        }
+        return res.json()
+      })
+      .then(res => console.log(res))
+      .catch(err => {
+        console.log(`Oh no! Fetch method got caught! error: \n \n ${err}`)
+      })
+  }
+
   render() {
     return <h1>Test</h1>
   }
--- src/server/index.js
+++ src/server/index.js
@@ -8,21 +8,25 @@ const buildPath = path.join(__dirname, '../../build')
 
 app.use(express.static(buildPath))
 
-app.get('/', function(req, res) {
+//path for responding to api call that won't interefere with react-router when implemented
+app.get('/api/mentors', (req, res) => {
+  fs.readFile(mentorsFilePath, 'utf8', (err, data) => {
+    console.log(data)
+    try {
+      console.log(data)
+      res.send(data)
+    } catch (error) {
+      throw new Error(err)
+    }
+  })
+})
+
+//catch-all path for sercing the build folder (the react app)
+app.get('/*', function(req, res) {
   const indexPath = path.join(buildPath, 'index.html')
   res.sendFile(indexPath)
 })
 
 app.listen(process.env.PORT || 9001, () => {
   console.log('Test')
-})
-
-app.get('/mentors', (req, res) => {
-  fs.readFile(mentorsFilePath, 'utf8', (err, data) => {
-    try {
-      res.send(data)
-    } catch (error) {
-      throw new Error(err)
-    }
-  })
 })
