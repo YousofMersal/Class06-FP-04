52d2e44e9df50fd7eb5199e4566e38c9b2876996
Yousof Mersal <yousof777@gmail.com>
2018-12-03 21:44:41 +0100

    add fetch validation

--- src/client/apiCalls.jsx
+++ src/client/apiCalls.jsx
@@ -1,7 +1,13 @@
 async function GetJsonFromApi() {
-  const response = await fetch('/api/mentors')
-  const body = await response.json()
-  return body
+  try {
+    //this code runs every time it tried to make a request
+    const response = await fetch('/api/mentors')
+    const body = await response.json()
+    return body
+  } catch (err) {
+    //this code runs only when an error happens.
+    throw new Error('Whoops, error in fetching data!: ' + err)
+  }
 }
 
 export { GetJsonFromApi }
