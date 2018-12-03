49da89d1b031fe7eb4d30fbe437696d24178b862
Yousof Mersal <yousof777@gmail.com>
2018-12-03 22:05:18 +0100

    add DBsettings endpoint for testing and later fetching

---
+++ src/server/dbsettings.js
@@ -0,0 +1,7 @@
+//Seperate settings for db to be used multiple places easier
+module.exports.settings = {
+  host: 'eu-cdbr-west-02.cleardb.net',
+  user: 'b01f968c466467',
+  password: '0f523869',
+  database: 'heroku_74a0a6dc4292ea3'
+}
