8f8796ab7dbfcf7b5357de4123a6224195074534
Yousof Mersal <yousof777@gmail.com>
2018-12-03 15:01:24 +0100

    add testview for displaying fetched data

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { GetJsonFromApi } from './apiCalls'
+import MentorsList from './MentorsList'
 
 export default class App extends Component {
   //adding mentors in state for displaying it later
@@ -13,20 +14,13 @@ export default class App extends Component {
   //fetch request to express backend
   componentDidMount() {
     GetJsonFromApi().then(res => this.setState({ mentors: res }))
-    // fetch('/api/mentors')
-    //   .then(res => {
-    //     if (res.status !== 200) {
-    //       throw Error(res.statusText)
-    //     }
-    //     return res.json()
-    //   })
-    //   .then(res => console.log(res))
-    //   .catch(err => {
-    //     console.log(`Oh no! Fetch method got caught! error: \n \n ${err}`)
-    //   })
   }
 
   render() {
-    return <h1>Test</h1>
+    return (
+      <div>
+        <MentorsList data={this.state.mentors} />
+      </div>
+    )
   }
 }
