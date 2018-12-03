b79608e0ef4bb78680cfa0f8c95ed00780356f44
Yousof Mersal <yousof777@gmail.com>
2018-12-03 22:58:13 +0100

    display data fetched from db

--- src/client/apiCalls.jsx
+++ src/client/apiCalls.jsx
@@ -1,13 +1,13 @@
-async function GetJsonFromApi() {
+async function getMentorsFromDb() {
   try {
     //this code runs every time it tried to make a request
-    const response = await fetch('/api/mentors')
+    const response = await fetch('/api/dbtest')
     const body = await response.json()
     return body
   } catch (err) {
     //this code runs only when an error happens.
-    throw new Error('Whoops, error in fetching data!: ' + err)
+    throw new Error('Whoops, error in fetching data from api!' + err)
   }
 }
 
-export { GetJsonFromApi }
+export { getMentorsFromDb }
--- src/client/components/MentorsList.jsx
+++ src/client/components/MentorsList.jsx
@@ -9,6 +9,9 @@ export default class MentorsList extends Component {
           <MentorsNameView
             firstName={item.first_name}
             lastName={item.last_name}
+            slackNickname={item.slack_nickname}
+            type={item.type}
+            status={item.status}
             key={item.id}
           />
         ))}
