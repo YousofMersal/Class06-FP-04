3c69fb39e332d0ef9b2ada61c5981c3a6c4dded6
Yousof Mersal <yousof777@gmail.com>
2018-12-03 22:05:18 +0100

    add DBsettings endpoint for testing and later fetching

--- src/server/index.js
+++ src/server/index.js
@@ -2,11 +2,30 @@ const express = require('express')
 const path = require('path')
 const fs = require('fs')
 const app = express()
-
+const mysql = require('mysql')
+const dbsettings = require('./dbsettings')
 const mentorsFilePath = path.join(__dirname, './api/Mentors.json')
 const buildPath = path.join(__dirname, '../../build')
 
 app.use(express.static(buildPath))
+
+app.get('/api/dbtest', (req, res) => {
+  //creat connection to HerokuDB
+  const connection = mysql.createConnection(dbsettings.settings)
+  connection.connect()
+
+  //Query to the database and send it back in the response to the client
+  connection.query('DESC mentors', (err, data, fields) => {
+    if (err) {
+      throw new Error()
+    } else {
+      res.send(data)
+    }
+  })
+
+  //disconecting so we don't create errors with the HerokuDB
+  connection.end()
+})
 
 //path for responding to api call that won't interefere with react-router when implemented
 app.get('/api/mentors', (req, res) => {
@@ -19,7 +38,7 @@ app.get('/api/mentors', (req, res) => {
   })
 })
 
-//catch-all path for sercing the build folder (the react app)
+//catch-all path for serving the build folder (the react app)
 app.get('/*', function(req, res) {
   const indexPath = path.join(buildPath, 'index.html')
   res.sendFile(indexPath)
